5d3900dd0f418798551121b1ee0a5cd26af5c9a9
IT Service <itservice@WynnSoft-Solution.com>
2023-09-27 09:41:29 +0700

    add ep one piece ss20

--- src/data/series_2023.js
+++ src/data/series_2023.js
@@ -82,6 +82,81 @@ export const series_2023 = [
         title: "episode 900",
         video: "https://drive.google.com/file/d/17jxbHlrxP6Uwmfj9xd6qYyfbpNfZqMEC/preview",
       },
+      {
+        ep: 901,
+        title: "episode 901",
+        video: "https://drive.google.com/file/d/1Kx0HA5IClHk3VAfXbGpvuVxSltCOheL2/preview",
+      },
+      {
+        ep: 902,
+        title: "episode 902",
+        video: "https://drive.google.com/file/d/1nlq-3z-6CcILrC8DSBjoF6lDa6-EjKA9/preview",
+      },
+      {
+        ep: 903,
+        title: "episode 903",
+        video: "https://drive.google.com/file/d/1AHS3nxLpS4G5-RBROhdwqYuL71md5vqy/preview",
+      },
+      {
+        ep: 904,
+        title: "episode 904",
+        video: "https://drive.google.com/file/d/1qTYGvRVsdwPikG6qujzff-vcRJyzwRsv/preview",
+      },
+      {
+        ep: 905,
+        title: "episode 905",
+        video: "https://drive.google.com/file/d/1hmsx5f7b1xHjnEOc20cFIvuYlzT6zzrl/preview",
+      },
+      {
+        ep: 906,
+        title: "episode 906",
+        video: "https://drive.google.com/file/d/10bKDySjmw4wdi1OUC6n3g0jalT5jUc6Q/preview",
+      },
+      {
+        ep: 907,
+        title: "episode 907",
+        video: "https://drive.google.com/file/d/10c8ikPpMpfkQgjQBIeLlez9F7RJK0wMg/preview",
+      },
+      {
+        ep: 908,
+        title: "episode 908",
+        video: "https://drive.google.com/file/d/11PfRiMxL1lCMOKHET15ds377NiWtcfbJ/preview",
+      },
+      {
+        ep: 909,
+        title: "episode 909",
+        video: "https://drive.google.com/file/d/1O35h0xhFrb0RmVjye7fwGEPigcbiG1ti/preview",
+      },
+      {
+        ep: 910,
+        title: "episode 910",
+        video: "https://drive.google.com/file/d/1VphDTnMSbx_yCXsJDFvweElfWYPr605C/preview",
+      },
+      {
+        ep: 911,
+        title: "episode 911",
+        video: "https://drive.google.com/file/d/1_54zYgE4gZD7xIxWLfecY1vBV-UgNvqb/preview",
+      },
+      {
+        ep: 912,
+        title: "episode 912",
+        video: "https://drive.google.com/file/d/1jeWebxxS5T91CphOtoKyDdtSHCvLrfP-/preview",
+      },
+      {
+        ep: 913,
+        title: "episode 913",
+        video: "https://drive.google.com/file/d/1VldCGgTs7oCLClPrPoVnY4WuFxH216ww/preview",
+      },
+      {
+        ep: 914,
+        title: "episode 914",
+        video: "https://drive.google.com/file/d/1RNrLV5nILZFjAa6Y57_ZMdJ7dk0dKgO9/preview",
+      },
+      {
+        ep: 915,
+        title: "episode 915",
+        video: "https://drive.google.com/file/d/1t_utExbrJxlFW3frktuGA0js5mtEIP9Y/preview",
+      },
     ],
   },
 ];
