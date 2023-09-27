c06d8f479d1b8be1ccf8e5d7973c76c9540a59c5
IT Service <itservice@WynnSoft-Solution.com>
2023-09-27 17:27:07 +0700

    update loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,24 @@ import { useEffect, useState } from "react";
 import Login from "./layouts/Login";
 import Register from "./layouts/Register";
 import { useAccount } from "./context/AccountContext";
+import { PulseLoader } from "react-spinners";
 
 function App() {
+  const [loading, setLoading] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(
     localStorage.getItem("auth") || false
   );
   const auth = localStorage.getItem("auth");
   const location = useLocation();
   const { accounts } = useAccount();
+
+  useEffect(() => {
+    setLoading(true);
+    const timeoutId = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+    return () => clearTimeout(timeoutId);
+  }, []);
 
   useEffect(() => {
     const existingAccountsString = localStorage.getItem("accounts");
@@ -30,35 +40,48 @@ function App() {
 
   return (
     <>
-      {isLoggedIn == "true" || auth == "true" ? (
-        <>
-          <Header setIsLoggedIn={setIsLoggedIn} />
-          <main
-            className={`w-full h-[calc(100vh-60px)] ${
-              location.pathname == "/"
-                ? "bg-black"
-                : "bg-[url('/images/main_background.png')]"
-            }  bg-cover overflow-x-hidden overflow-y-auto`}
-          >
-            <div
-              className={`${location.pathname == "/" ? "p-0" : "p-4 xl:p-12"}`}
-            >
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/movies" element={<Movies />} />
-                <Route path="/movies/:movieID" element={<MoviesDetail />} />
-                <Route path="/series" element={<Series />} />
-                <Route path="/series/:serieID" element={<SeriesDetail />} />
-              </Routes>
-            </div>
-          </main>
-        </>
+      {loading ? (
+        <div className="bg-[url('/images/main_background.png')] bg-cover w-full h-screen flex justify-center items-center">
+          <PulseLoader color="#DB0000" size={30} />
+        </div>
       ) : (
-        <Routes>
-          <Route path="/" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
+        <>
+          {isLoggedIn == "true" || auth == "true" ? (
+            <>
+              <Header setIsLoggedIn={setIsLoggedIn} />
+              <main
+                className={`w-full h-[calc(100vh-60px)] ${
+                  location.pathname == "/"
+                    ? "bg-black"
+                    : "bg-[url('/images/main_background.png')]"
+                }  bg-cover overflow-x-hidden overflow-y-auto`}
+              >
+                <div
+                  className={`${
+                    location.pathname == "/" ? "p-0" : "p-4 xl:p-12"
+                  }`}
+                >
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/movies" element={<Movies />} />
+                    <Route path="/movies/:movieID" element={<MoviesDetail />} />
+                    <Route path="/series" element={<Series />} />
+                    <Route path="/series/:serieID" element={<SeriesDetail />} />
+                  </Routes>
+                </div>
+              </main>
+            </>
+          ) : (
+            <Routes>
+              <Route
+                path="/"
+                element={<Login setIsLoggedIn={setIsLoggedIn} />}
+              />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          )}
+        </>
       )}
     </>
   );
